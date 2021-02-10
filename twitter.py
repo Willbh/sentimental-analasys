d9ffa9edf11f2d2171b2fd637eb5ec39ce966153
WIlliam <31324102+Willbh@users.noreply.github.com>
2021-02-10 22:32:48 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -23,7 +23,7 @@ class TwitterClient(object):
             self.retweets_only = retweets_only
             self.with_sentiment = with_sentiment
             self.api = tweepy.API(self.auth)
-            self.tweet_count_max = 100  # To prevent Rate Limiting
+            self.tweet_count_max = 100000  # To prevent Rate Limiting
         except:
             print("Error: Authentication Failed")
 
